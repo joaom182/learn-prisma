c97691944d9b7316817e15394d6c5adb86173c98
João Mainka <joaom182@hotmail.com>
2021-10-03 17:58:19 -0300

    Fix Docker build issues

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,9 @@
-FROM node:alpine
-
-RUN apk add --no-cache --virtual .build-deps \
-    ca-certificates \
-    wget \
-    tar && \
-    cd /usr/local/bin && \
-    wget https://yarnpkg.com/latest.tar.gz && \
-    tar zvxf latest.tar.gz && \
-    ln -s /usr/local/bin/dist/bin/yarn.js /usr/local/bin/yarn.js && \
-    apk del .build-deps
+FROM node:lts
 
 WORKDIR /usr/app
 
 COPY . .
 RUN yarn
-RUN yarn build
+RUN yarn dist
 
 CMD ["yarn", "start"]
---
+++ dist/main.js
@@ -0,0 +1,70 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _client = require("@prisma/client");
+
+const prisma = new _client.PrismaClient();
+
+async function createUser() {
+  return prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: {
+          title: 'Hello World'
+        }
+      },
+      profile: {
+        create: {
+          bio: 'I like turtles'
+        }
+      }
+    }
+  });
+}
+
+async function getAllUsers() {
+  return prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true
+    }
+  });
+}
+
+async function updatePost() {
+  return prisma.post.updateMany({
+    where: {},
+    data: {
+      published: true
+    }
+  });
+}
+
+async function cleanupDb() {
+  await prisma.post.deleteMany();
+  await prisma.profile.deleteMany();
+  await prisma.user.deleteMany();
+}
+
+async function main() {
+  await cleanupDb();
+  await createUser();
+  console.dir(await getAllUsers(), {
+    depth: null
+  });
+  console.dir(await updatePost());
+  console.dir(await getAllUsers(), {
+    depth: null
+  });
+}
+
+main().catch(e => {
+  throw e;
+}).finally(async () => {
+  await prisma.$disconnect();
+});
